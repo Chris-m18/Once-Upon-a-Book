35dfc241fd5d71afab67c732df32bf138b66c4a7
Christian Matos <chris@Christians-MBP.fios-router.home>
2020-07-14 14:26:49 -0400

    seed complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  post '/auth/login', to: 'authentication#login'
+  get '/auth/verify', to: 'authentication#verify'
+  resources :categories
+  resources :books
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
